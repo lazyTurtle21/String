2cdf4222e3e32db74c14a6da2015cb99749350b8
YevheniiMoroz <35136884+YevheniiMoroz@users.noreply.github.com>
2018-10-20 15:58:26 +0300

    some functions were added and updated
    added functions: strlen, my_str_from_cstr, my_str_size, my_str_getc, my_str_insert_c, my_str_get_cstr
    updated functions: my_str_create

--- main.c
+++ main.c
@@ -10,6 +10,16 @@ typedef struct {
 
 } my_str_t;
 
+//? -3 -- null pointer exception
+//? рахує кількість символів без останнього ('\0')
+const size_t strlen(const char* str) {
+	if (!str)
+		return -3;
+
+	int length = 0;
+	while (*(str + ++length)) {}
+	return length;
+}
 
 //! Створити стрічку із буфером вказаного розміру. Пам'ять виділяється динамічно.
 //! Варто виділяти buf_size+1 для спрощення роботи my_str_get_cstr().
@@ -30,13 +40,45 @@ int my_str_create(my_str_t* str, size_t buf_size) {
 //! менший за її розмір -- вважати помилкою.
 //! Пам'ять виділяється динамічно.
 //! 0 -- якщо все ОК, -1 -- недостатній розмір буфера, -2 -- не вдалося виділити пам'ять
-int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size);
+
+//? -3 -- null pointer exception
+//? чи вартує в цій функції використати my_str_create?
+//? чи треба тут (str->capacity_m = str->size_m + 1) виділяти +1 резервний байт місця?
+//? чи треба записувати '\0' в кінець str->data?
+int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size) {
+	size_t str_length = strlen(cstr);
+
+	if (!str) 
+		return -3;
+	if (buf_size < str_length)
+		return -1;
+
+	str->size_m = str_length;
+	if (buf_size <= 0)
+		str->capacity_m = str->size_m + 1;
+	else
+		str->capacity_m = buf_size;
+	str->data = malloc(buf_size);
+
+	if (!str->data)
+		return -2;
+
+	for (int i = 0; i < str->size_m; i++)
+		*(str->data + i) = *(cstr + i);
+
+	return 0;
+}
 
 //! Звільнє пам'ять, знищуючи стрічку:
 void my_str_free(my_str_t* str);
 
 //! Повертає розмір стрічки:
-size_t my_str_size(const my_str_t* str);
+//? -3 -- null pointer exception
+size_t my_str_size(const my_str_t* str) {
+	if (!str)
+		return -3;
+	return str->size_m;
+}
 
 //! Повертає розмір буфера:
 size_t my_str_capacity(const my_str_t* str);
@@ -46,7 +88,16 @@ int my_str_empty(const my_str_t* str);
 
 //! Повертає символ у вказаній позиції, або -1, якщо вихід за межі стрічки
 //! Тому, власне, int а не char
-int my_str_getc(const my_str_t* str, size_t index);
+//? -3 -- null pointer exception
+int my_str_getc(const my_str_t* str, size_t index) {
+	if (!str)
+		return -3;
+
+	if (index < 0 || index >= str->size_m)
+		return -1;
+	
+	return *(str->data + index);
+}
 
 //! Записує символ у вказану позиції (заміняючи той, що там був),
 //! Повертає 0, якщо позиція в межах стрічки,
@@ -72,7 +123,21 @@ void my_str_clear(my_str_t* str);
 
 //! Вставити символ у стрічку в заданій позиції, змістивши решту символів праворуч.
 //! Якщо це неможливо, повертає -1, інакше 0.
-int my_str_insert_c(my_str_t* str, char c, size_t pos);
+//? -3 -- null pointer exception
+int my_str_insert_c(my_str_t* str, char c, size_t pos) {
+	if (str->size_m >= str->capacity_m)
+		return -1;
+	if (!str)
+		return -3;
+
+	int size = str->size_m;
+	while (size-- != pos) 
+		*(str->data + size + 1) = *(str->data + size);
+	*(str->data + pos) = c;
+	str->size_m++;
+
+	return 0;
+}
 
 //! Вставити стрічку в заданій позиції, змістивши решту символів праворуч.
 //! Якщо це неможливо, повертає -1, інакше 0.
@@ -106,7 +171,10 @@ int my_str_substr(const my_str_t* str, const char* to, size_t beg, size_t end);
 //! Вважатимемо, що змінювати цю С-стрічку заборонено.
 //! Якщо в буфері було зарезервовано на байт більше за макс. розмір, можна
 //! просто додати нульовий символ в кінці та повернути вказівник data.
-const char* my_str_get_cstr();
+const char* my_str_get_cstr(my_str_t* str) {
+	*(str->data + 1) = '\0';
+	return str->data;
+}
 
 //! Знайти першу підстрічку в стрічці, повернути номер її
 //! початку або -1u, якщо не знайдено. from -- місце, з якого починати шукати.
